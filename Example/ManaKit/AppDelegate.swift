b013f2d9e936944f559dceae8baac3ebd763e852
Jovito Royeca <jovit.royeca@gmail.com>
2017-08-06 22:48:52 +0800

    feat: Added CMRule and CMGlossary managed objects.

--- Example/ManaKit/AppDelegate.swift
+++ Example/ManaKit/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // create the database...
 //        DatabaseMaintainer.sharedInstance.json2CoreData()
 //        DatabaseMaintainer.sharedInstance.updateMCINumbers()
-        DatabaseMaintainer.sharedInstance.tempUpdateCardNumberOrder()
+        DatabaseMaintainer.sharedInstance.rules2CoreData()
         
         // ... or test the database
 //        ManaKit.sharedInstance.setupResources()
---
+++ ManaKit/Classes/CMGlossary+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CMGlossary+CoreDataClass.swift
+//  
+//
+//  Created by Jovito Royeca on 06/08/2017.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CMGlossary)
+public class CMGlossary: NSManagedObject {
+
+}
