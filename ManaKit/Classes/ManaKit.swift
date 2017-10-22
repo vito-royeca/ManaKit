38cac86dd997410069561ea422339d929e2c1b73
Jovito Royeca <jovit.royeca@gmail.com>
2017-10-22 10:00:26 +0800

    Release 1.0.7.

--- ManaKit/Classes/ManaKit.swift
+++ ManaKit/Classes/ManaKit.swift
@@ -326,13 +326,17 @@ open class ManaKit: NSObject {
                 for url in urls {
                     let data = try! Data(contentsOf: url)
                     let error: UnsafeMutablePointer<Unmanaged<CFError>?>? = nil
-                    guard let provider = CGDataProvider(data: data as CFData) else { return }
-                    guard let font = CGFont(provider) else { return }
-                    
-                    if !CTFontManagerRegisterGraphicsFont(font, error) {
-                        if let unmanagedError = error?.pointee {
-                            if let errorDescription = CFErrorCopyDescription(unmanagedError.takeUnretainedValue()) {
-                                print("Failed to load font: \(errorDescription)")
+
+                    if let provider = CGDataProvider(data: data as CFData) {
+                        let font = CGFont(provider)
+                        
+                        if let font = font {
+                            if !CTFontManagerRegisterGraphicsFont(font, error) {
+                                if let unmanagedError = error?.pointee {
+                                    if let errorDescription = CFErrorCopyDescription(unmanagedError.takeUnretainedValue()) {
+                                        print("Failed to load font: \(errorDescription)")
+                                    }
+                                }
                             }
                         }
                     }
