aef4f7862a7107be3f263b943a2364c448770dfc
Jovito Royeca <jovit.royeca@gmail.com>
2018-06-21 01:05:04 +0800

    fix: Handling \n and empty space in NSAttributedString.addSymbols(:)

--- ManaKit/Classes/NSAttributedString+Utilities.swift
+++ ManaKit/Classes/NSAttributedString+Utilities.swift
@@ -13,10 +13,10 @@ public extension NSAttributedString {
         let newAttributedString = NSMutableAttributedString()
         let text = self.string.trimmingCharacters(in: CharacterSet.whitespaces)
         var fragmentText = NSMutableString()
-        var offset = 0
+        var sentinel = 0
         
         repeat {
-            for i in offset...text.count - 1 {
+            for i in sentinel...text.count - 1 {
                 let c = text[text.index(text.startIndex, offsetBy: i)]
                 
                 if c == "{" {
@@ -24,66 +24,69 @@ public extension NSAttributedString {
                     
                     for j in i...text.count - 1 {
                         let cc = text[text.index(text.startIndex, offsetBy: j)]
+                        symbol.append(String(cc))
                         
                         if cc == "}" {
-                            offset = j + 1
+                            sentinel = j + 1
                             break
-                        } else {
-                            symbol.append(String(cc))
                         }
                     }
                     
-                    var cleanSymbol = symbol.replacingOccurrences(of: "{", with: "")
-                        .replacingOccurrences(of: "}", with: "")
-                        .replacingOccurrences(of: "/", with: "")
-                    
-                    if cleanSymbol == "CHAOS" {
-                        cleanSymbol = "Chaos"
-                    }
-                    
-                    if let image = ManaKit.sharedInstance.symbolImage(name: cleanSymbol as String) {
-                        let imageAttachment =  NSTextAttachment()
-                        imageAttachment.image = image
-                        
-                        var width = CGFloat(16)
-                        let height = CGFloat(16)
-                        var imageOffsetY = CGFloat(0)
-                        
-                        if cleanSymbol == "100" {
-                            width = 35
-                        } else if cleanSymbol == "1000000" {
-                            width = 60
-                        }
-                        
-                        if height > pointSize {
-                            imageOffsetY = -(height - pointSize) / 2.0
-                        } else {
-                            imageOffsetY = -(pointSize - height) / 2.0
-                        }
-                        
-                        imageAttachment.bounds = CGRect(x: 0, y: imageOffsetY, width: width, height: height)
-                        
-                        let attachmentString = NSAttributedString(attachment: imageAttachment)
-                        let attributedString = NSMutableAttributedString(string: fragmentText as String)
-                        attributedString.append(attachmentString)
-                        
-                        newAttributedString.append(attributedString)
-                        fragmentText = NSMutableString()
-                        break
-                    }
-                    
+                    let attributedString = NSMutableAttributedString(string: fragmentText as String)
+                    attributedString.append(NSAttributedString(symbol: symbol as String, pointSize: pointSize))
+                    newAttributedString.append(attributedString)
+                    fragmentText = NSMutableString()
+                    break
+                   
                 } else {
                     fragmentText.append(String(c))
-                    offset = i
+                    sentinel += 1
                 }
                 
             }
-        } while offset <= text.count - 1
+        } while sentinel <= text.count - 1
         
         let attributedString = NSMutableAttributedString(string: fragmentText as String)
         newAttributedString.append(attributedString)
         
         return newAttributedString
+    }
+    
+    public convenience init(symbol: String, pointSize: CGFloat) {
+        var cleanSymbol = symbol.replacingOccurrences(of: "{", with: "")
+            .replacingOccurrences(of: "}", with: "")
+            .replacingOccurrences(of: "/", with: "")
+        
+        if cleanSymbol == "CHAOS" {
+            cleanSymbol = "Chaos"
+        }
+        
+        guard let image = ManaKit.sharedInstance.symbolImage(name: cleanSymbol as String) else {
+            self.init()
+            return
+        }
+        
+        let imageAttachment =  NSTextAttachment()
+        imageAttachment.image = image
+        
+        var width = CGFloat(16)
+        let height = CGFloat(16)
+        var imageOffsetY = CGFloat(0)
+        
+        if cleanSymbol == "100" {
+            width = 35
+        } else if cleanSymbol == "1000000" {
+            width = 60
+        }
+        
+        if height > pointSize {
+            imageOffsetY = -(height - pointSize) / 2.0
+        } else {
+            imageOffsetY = -(pointSize - height) / 2.0
+        }
+        
+        imageAttachment.bounds = CGRect(x: 0, y: imageOffsetY, width: width, height: height)
+        self.init(attachment: imageAttachment)
     }
     
     public func convertToHtml() -> NSMutableAttributedString? {
