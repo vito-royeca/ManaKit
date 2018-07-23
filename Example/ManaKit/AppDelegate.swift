eb7042635dbfaa35604a3ea6219566b8b90195ca
Jovito Royeca <jovit.royeca@gmail.com>
2018-07-23 20:19:51 +0800

    update: Keyrune to v3.2.2.

--- Example/ManaKit/AppDelegate.swift
+++ Example/ManaKit/AppDelegate.swift
@@ -36,8 +36,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //        DatabaseMaintainer.sharedInstance.updateLegalities()
 //        DatabaseMaintainer.sharedInstance.rules2CoreData()
         
-         // Temporary updates
-        
           // Normal run
         ManaKit.sharedInstance.setupResources()
         ManaKit.sharedInstance.configureTCGPlayer(partnerKey: "ManaGuide", publicKey: "A49D81FB-5A76-4634-9152-E1FB5A657720", privateKey: nil)
