2906f36c6bc8b5ac80cc131b22649fc9c64e40be
Vito Royeca <vito.royeca@gmail.com>
2022-03-21 18:53:32 -0400

    Fixed loading of images

--- Sources/ManaKit/ManaKit+Imaging.swift
+++ Sources/ManaKit/ManaKit+Imaging.swift
@@ -17,19 +17,7 @@ extension ManaKit {
     }
 
     public func symbolImage(name: String) -> UIImage? {
-//        let bundle = Bundle(for: ManaKit.self)
-//
-//        guard let bundleURL = bundle.resourceURL?.appendingPathComponent("ManaKit.bundle"),
-//            let resourceBundle = Bundle(url: bundleURL) else {
-//            return nil
-//        }
-//
-//        return UIImage(named: name, in: resourceBundle, compatibleWith: nil)
-        if let path = Bundle.module.path(forResource: name, ofType: "png") {
-            return UIImage(contentsOfFile: path)
-        } else {
-            return nil
-        }
+        return UIImage(named: name, in: Bundle.module, compatibleWith: nil)
     }
     
 //    public func downloadImage(ofCard card: MGCard, type: CardImageType, faceOrder: Int) -> Promise<Void> {
