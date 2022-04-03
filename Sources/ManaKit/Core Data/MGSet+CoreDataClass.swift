e7a953455ba41b988842c8cc1a049c37fc9d4127
Vito Royeca <vito.royeca@gmail.com>
2022-04-03 12:50:46 -0400

    Fixed set.sortedLanguages

--- Sources/ManaKit/Core Data/MGSet+CoreDataClass.swift
+++ Sources/ManaKit/Core Data/MGSet+CoreDataClass.swift
@@ -8,47 +8,47 @@
 import CoreData
 
 public class MGSet: MGEntity {
-    public var sortedLanguageCodes: [String]? {
+    public var sortedLanguages: [MGLanguage]? {
         guard let set = languages,
             let array = set.allObjects as? [MGLanguage] else {
             return nil
         }
 
         let arrayMap = array.map { $0.code }
-        var sortedArray = [String]()
+        var sortedArray = [MGLanguage]()
         
         if arrayMap.contains("en") {
-            sortedArray.append("EN")
+            sortedArray.append(contentsOf: array.filter({ $0.code == "en"}))
         }
         if arrayMap.contains("es") {
-            sortedArray.append("ES")
+            sortedArray.append(contentsOf: array.filter({ $0.code == "es"}))
         }
         if arrayMap.contains("fr") {
-            sortedArray.append("FR")
+            sortedArray.append(contentsOf: array.filter({ $0.code == "fr"}))
         }
         if arrayMap.contains("de") {
-            sortedArray.append("DE")
+            sortedArray.append(contentsOf: array.filter({ $0.code == "de"}))
         }
         if arrayMap.contains("it") {
-            sortedArray.append("IT")
+            sortedArray.append(contentsOf: array.filter({ $0.code == "it"}))
         }
         if arrayMap.contains("pt") {
-            sortedArray.append("PT")
+            sortedArray.append(contentsOf: array.filter({ $0.code == "pt"}))
         }
         if arrayMap.contains("ja") {
-            sortedArray.append("JA")
+            sortedArray.append(contentsOf: array.filter({ $0.code == "ja"}))
         }
         if arrayMap.contains("ko") {
-            sortedArray.append("KO")
+            sortedArray.append(contentsOf: array.filter({ $0.code == "ko"}))
         }
         if arrayMap.contains("ru") {
-            sortedArray.append("RU")
+            sortedArray.append(contentsOf: array.filter({ $0.code == "ru"}))
         }
         if arrayMap.contains("zhs") {
-            sortedArray.append("汉语")
+            sortedArray.append(contentsOf: array.filter({ $0.code == "zhs"}))
         }
         if arrayMap.contains("zht") {
-            sortedArray.append("漢語")
+            sortedArray.append(contentsOf: array.filter({ $0.code == "zht"}))
         }
         
         return sortedArray
