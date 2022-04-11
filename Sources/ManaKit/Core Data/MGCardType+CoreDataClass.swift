06f2afc7b8facbb25719d26a472b00202983a7bf
Vito Royeca <vito.royeca@gmail.com>
2022-04-10 21:31:50 -0400

    Adds cardtype.sortedChildren

--- Sources/ManaKit/Core Data/MGCardType+CoreDataClass.swift
+++ Sources/ManaKit/Core Data/MGCardType+CoreDataClass.swift
@@ -8,5 +8,13 @@
 import CoreData
 
 public class MGCardType: MGEntity {
-
+    public var sortedChildren: [MGCardType]? {
+        guard let set = children,
+            let array = set.allObjects as? [MGCardType] else {
+            return nil
+        }
+        
+        let sortedArray = array.sorted { ($0.name ?? "") < ($1.name ?? "")}
+        return sortedArray.isEmpty ? nil : sortedArray
+    }
 }
