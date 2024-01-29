b939aab82dc0478b5d34e7c3ae6d8f74316da19b
Vito Royeca <vito.royeca@gmail.com>
2024-01-29 18:05:59 -0500

    Updated iOS to v16; SwiftTools to 5.9, ZipArchive to 2.5.5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "ManaKit",
     platforms: [
         .macOS(.v12),
-        .iOS(.v15)
+        .iOS(.v16)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -20,7 +20,7 @@ let package = Package(
         //.package(url: /* package url */, from: "1.0.0"),
 //        .package(url: "https://github.com/tid-kijyun/Kanna.git", from: "5.2.2"),
         //.package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.1.0"),
-        .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: "2.4.2"),
+        .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: "2.5.5"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
