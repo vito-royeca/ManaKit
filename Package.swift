9e34a9e8beb57d16b7c92092a85c73564100a5c0
Vito Royeca <vito.royeca@gmail.com>
2024-01-29 17:32:36 -0500

    Updated iOS target to .v15

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "ManaKit",
     platforms: [
         .macOS(.v12),
-        .iOS("15.5")
+        .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
