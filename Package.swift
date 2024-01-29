f7efc9f910e4abb064165adfbe9e8f0d9b258ef9
Vito Royeca <vito.royeca@gmail.com>
2024-01-29 17:56:34 -0500

    Reverted ZipArchive to 2.4.2

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         //.package(url: /* package url */, from: "1.0.0"),
 //        .package(url: "https://github.com/tid-kijyun/Kanna.git", from: "5.2.2"),
         //.package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.1.0"),
-        .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: "2.5.5"),
+        .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: "2.4.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
